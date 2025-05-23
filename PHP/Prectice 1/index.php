c743a89ea856b0bb8a745e7f23cd5c28ebf40636
CYBER BOY ツ <mmursith12345@gmail.com>
2025-05-23 11:26:11 +0530

    PHP Intro

---
+++ PHP/Prectice 1/index.php
@@ -0,0 +1,41 @@
+
+<?php
+
+  echo " Php inai ivvaru meleum podalam";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Intro</title>
+</head>
+<body>
+
+<h1>
+<?php                               // php il enthatag um use panlam 
+                                     #  entha idaththilum use panlam
+  echo "HI Buddy";                  /* athatkul html ,css inaium run panlam */
+
+?>
+</h1>
+
+<?php
+
+  echo "What Are You Doing";        // php ulle html/css run panna mudiyathu ex:- <br>
+  echo "<br>";
+  echo 'have a nice day';             // "" or '' ithil 2 um use panlam
+
+?>
+
+<?php
+
+echo '<h1> Change H1 Tag </h1>'
+
+?>
+
+
+</body>
+</html>
