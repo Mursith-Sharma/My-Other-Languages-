9c3516a00c8812b5f0ac85e5f3971921bfcebdc4
CYBER BOY ツ <mmursith12345@gmail.com>
2025-06-05 13:26:36 +0530

    commends.sql

--- QUERY /basic/commends.sql
+++ QUERY /basic/commends.sql
@@ -7,8 +7,22 @@ DROP TABLE mursith;
 USE database_name;
 CREATE TABLE students_info                         -- create table for database_name
 (
-    st_id integer NOT Null,
+    st_id integer NOT Null,                       -- keywords capital & small does'nt matter ( varchar, integer ,null ,..etc)
     st_name VARCHAR (20) NOT null,
     st_email varchar (40) NOT null,
     PRIMARY KEY (st_id)
 );
+
+------------------------------------------------------------------------------------------------------
+
+ALTER TABLE students_info                         -- ivvaru marupadium column add panna mudium
+ADD st_mobile integer ;
+
+------------------------------------------------------------------------------------------------------
+
+ALTER TABLE students_info MODIFY COLUMN st_mobile          -- students_info ilulla st_mobile inai modify pannal
+CHARACTER (20) ;
+
+------------------------------------------------------------------------------------------------------
+
+RENAME TABLE students_info to students_information;        --rename
