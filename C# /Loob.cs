22eef4786d83996d0a2924e588d16e224e79fee0
CYBER BOY ツ <mmursith12345@gmail.com>
2025-06-21 10:30:55 +0530

    Update Loob.cs

--- C# /Loob.cs
+++ C# /Loob.cs
@@ -60,3 +60,100 @@ namespace MyBasicconsoleapp
     }
 }
 
+---------------------------------------------     Do While Loob        ----------------------------------------------------
+
+using System;
+
+namespace MyBasicconsoleapp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            do
+            {
+                Console.WriteLine(a);
+                a++;
+            }
+            while (a < 50);
+            Console.ReadKey();
+        }
+
+    }
+}
+
+---------------------------------------------     For Loob        ----------------------------------------------------
+
+using System;
+
+namespace MyBasicconsoleapp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            for(int a=0; a <= 20; a++)
+            {
+                Console.WriteLine("Hi");
+            }
+            Console.ReadKey();
+        }
+
+    }
+}
+
+-----------------------------------------------------------------------------------------------------
+
+using System;
+
+namespace MyBasicconsoleapp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Your number is " + a);
+
+            for (; a<50; a++)
+            {
+                Console.WriteLine(a);
+            }
+            Console.ReadKey();
+        }
+
+    }
+}
+
+-----------------------------------------------------------------------------------------------------
+
+
+using System;
+
+namespace MyBasicconsoleapp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Your number is " + a);
+
+            for (; a>3; a--)                               //enter 3 kku mele ulla number
+            {
+                Console.WriteLine(a);
+            }
+            Console.ReadKey();
+        }
+
+    }
+}
+
+
+
+
