da97c7770fb8e19085871e0b3c25e760ead8181f
CYBER BOY ツ <mmursith12345@gmail.com>
2025-06-24 22:13:19 +0530

    Create c# calculator create.cs

---
+++ C# /c# calculator create.cs
@@ -0,0 +1,6 @@
+1. create calculator using visual studio
+2. give that text name
+3. give that name in design ( for function name )
+4. double click that button or lable or text their go to function
+name change
+5. go to event to for name change. delete that name and enter, after change that name in name ( line 3 concept )
