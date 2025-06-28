aab5708cc5aff3953d6c7a319751d2cce5f5b05c
CYBER BOY ツ <mmursith12345@gmail.com>
2025-06-28 11:43:44 +0530

    Add files via upload

---
+++ Dot Net /prectice 2 ( Student data save & update )/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Windowsformprectical
+{
+    public partial class Form1 : Form
+    {
+
+        student studentobject = new student();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void savebtn_Click(object sender, EventArgs e)
+        {
+
+            studentobject.id =Convert.ToInt32 (idbox.Text);
+            studentobject.name = namebox.Text;
+            studentobject.age = Convert.ToInt32(agebox.Text);
+            studentobject.mobile = mobilebox.Text;
+            studentobject.save();
+        }
+
+        private void updatebtn_Click(object sender, EventArgs e)
+        {
+            studentobject.id = Convert.ToInt32(idbox.Text);
+            studentobject.name = namebox.Text;
+            studentobject.age = Convert.ToInt32(agebox.Text);
+            studentobject.mobile = mobilebox.Text;
+            studentobject.update();
+        }
+    }
+}
---
+++ Dot Net /prectice 2 ( Student data save & update )/student.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Windowsformprectical
+{
+    internal class student
+    {
+        public int id, age;
+        public string name, mobile;
+
+        public void save()
+        {
+            MessageBox.Show("save : [ id : " + id + ", Name : " + name + ", age : " + age + ", Mobile : " + mobile + "]");
+        }
+
+        public void update()
+        {
+            MessageBox.Show("Update : [ id : " + id + ", Name : " + name + ", age :" + age + ", Mobile : " + mobile + "]");
+        }
+    }
+
+
+
+}
