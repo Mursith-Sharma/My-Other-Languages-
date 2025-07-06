e945ef187f75ae648a7485a4c462e76866f25707
CYBER BOY ツ <mmursith12345@gmail.com>
2025-07-06 21:58:51 +0530

    Update Poscontroller.php

--- PHP LARAVEL/9. Variable Pass ( controller to view file )/Poscontroller.php
+++ PHP LARAVEL/9. Variable Pass ( controller to view file )/Poscontroller.php
@@ -15,4 +15,19 @@ class Poscontroller extends Controller
         return view('detail');
     }
 } 
+
+// ✅ $title = "Mars Tech";
+// 🔹 இது ஒரு PHP variable
+// 🔹 இதில் "Mars Tech" என்ற string assign பண்ணப்பட்டிருக்கு.
+// 🔹 இந்த variable-ஐ later-ஆ view-க்கு pass பண்ணபோகிறோம்.
+
+// ✅ return view('index', compact('title'));
+// 🔹 இது Laravel-ல் ஒரு view file-ஐ return பண்ணும் statement.
+// 🔹 view('index') என்றால் => resources/views/index.blade.php file-ஐ load பண்ணு என்பதற்காக.
+
+// 🔹 compact('title') என்றால்:
+
+// 'title' என்பது variable-ஐ key-ஆக convert பண்ணும்.
+
+// Laravel-க்கு $title என்ற variable-ஐ pass பண்ணும்.
 ?>
