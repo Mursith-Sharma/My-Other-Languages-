ec52ad12b85d746b7c60e61af9e125ab4cbc4225
CYBER BOY ツ <mmursith12345@gmail.com>
2025-07-06 11:01:20 +0530

    Update Explain.php

--- PHP LARAVEL/Explain.php
+++ PHP LARAVEL/Explain.php
@@ -1,3 +1,24 @@
+php laraval 
+1. install wamp or xamp
+2. install Laravel using power sell ( Laravel website il link irukkum athai windows power sell il paste panni install pannal)
+3. install node js  (check in cmd ->>>  node -v and npm -v )
+3. athil ellavatrin version parka mudium like -> php -v , composer -v Laravel
+4. go to Laravel folder , click path, type cmd, after cmd kku varum
+5. cmd il Which starter kit would you like to install? none , database kku sql kodukkavum, Default database updated. Would you like to run the default database migrations? no ( after we will fix )
+6. Would you like to run npm install and npm run build? no ( after we will fix ) <- ithatku node js install pannanum
+8. after go to your Laravel folder anha folder inai visuals studio il open pannu
+9. athil env file irukkum -> athil db entru ullathil database connection and password matra mudium ( must check )
+10. open terminal type this ->> npm install && npm run build	after ->> composer run dev ( server, queue worker, and Vite development server ivatrai start pannuvatathatku)
+11. sometime terminal il port il error varum athavathu port continiue aaga run agum. but error ellavittal ivvaru irukkum -> INFO  Server running on [http://127.0.0.1:8000]. 
+12. LARAVEL v12.19.3  plugin v1.3.0)  ithil run aagamal error varum vanthal your cmd il ->> php --ini ( ithil php configuration kkuriya path irukkum athai copy pannu )
+13. antha path inai file il paste pannaal notepad il open aagum .athil EGPCS inai GPCS aaha matri save panni pin run pannavum
+14. athilulla link inai browser il open pannavum like this ->> http://127.0.0.1:8000/
+15. after visual cmd ->> php artisan migrate -> enter yes
+16. neengal (composer run dev) visual cmd il ittal athil link kidaikkum. virumpiya neraththil opne panlam
+
+-------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
 | விஷயம்         | Already Learn panna PHP                                                        | Laravel                                         |
 | ---------------- | --------------------------------------------------------------------------     | ----------------------------------------------  |
 | File handling    | `index.php`, `about.php`, `contact.php` மாதிரி ஒவ்வொரு pageக்கும் ஒரு file | எல்லாம் `route`-களில் manage பண்ணலாம்     |
