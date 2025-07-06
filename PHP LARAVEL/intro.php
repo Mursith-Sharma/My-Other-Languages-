3e66a084dee5ccb900b22f68913f22a2597890a8
CYBER BOY ツ <mmursith12345@gmail.com>
2025-07-06 13:02:47 +0530

    Update intro.php

--- PHP LARAVEL/intro.php
+++ PHP LARAVEL/intro.php
@@ -53,32 +53,6 @@ route::get('/service',function(){
 
 });
 
--------------------------------- TimeOut  ------------------------------------------------------------------------------------------
-
-server aanathu 300s il auto aaga stop aagum athai thadukka -->>>  process time increase panlam or process time 0 pannalam 0 pannal server aanathu off aagathu
-
-process time increase 
-go to composer.json -> config option -> add this below-------------
-  
-{
-    "config": {
-        "process-timeout": 900                     // ithil time set panlam or 0 podalam
-    }
-}
-
-full code below-------------------
-  
-    "config": {
-        "optimize-autoloader": true,
-        "preferred-install": "dist",
-        "sort-packages": true,
-        "allow-plugins": {
-            "pestphp/pest-plugin": true,
-            "php-http/discovery": true
-        },
-        "process-timeout": 0
-    },
-
 ------------------------------------------------------------------------------------------------------------------
 
 naam inimel antha code il function use panna koodathu athu wrong athatku pathilaga controller use panna vendum
