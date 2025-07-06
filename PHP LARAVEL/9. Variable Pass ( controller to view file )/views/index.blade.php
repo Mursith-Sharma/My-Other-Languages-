5dd70749886b35dce734b30632356587791f572d
CYBER BOY ツ <mmursith12345@gmail.com>
2025-07-06 22:01:31 +0530

    Update index.blade.php

--- PHP LARAVEL/9. Variable Pass ( controller to view file )/views/index.blade.php
+++ PHP LARAVEL/9. Variable Pass ( controller to view file )/views/index.blade.php
@@ -1,6 +1,5 @@
-@dd($title)                                      
-@extends('layout.master')                      
-@section('box')
+@dd($title)                                     <!-- dd - dump & dive -->   
+@extends('layout.master')                       <!--$title என்ற variable il ullavatrai browser-ல் print பண்ணும் (array, string, object எல்லாமே).           -->
 
     <div class="container-fluid ">
         </div>
