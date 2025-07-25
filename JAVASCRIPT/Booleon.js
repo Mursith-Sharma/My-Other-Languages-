5370937eb2946dfb5fe32f176f2d52548d518c7f
CYBER BOY ツ <mmursith12345@gmail.com>
2025-07-25 14:09:04 +0530

    Create Booleon.js

---
+++ JAVASCRIPT/Booleon.js
@@ -0,0 +1,10 @@
+let payment =true;
+
+if(payment){
+    console.log("payment done");                    //if condition always check true & print that
+
+}
+else{
+    console.log("payment not valid");
+}
+/////////////////////////////////////////////
