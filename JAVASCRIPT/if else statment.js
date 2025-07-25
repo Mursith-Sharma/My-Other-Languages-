76507ee28d4e61f022bc0c092a0b48a12975a778
CYBER BOY ツ <mmursith12345@gmail.com>
2025-07-25 17:20:20 +0530

    Update and rename Booleon.js to if else statment.js

---
+++ JAVASCRIPT/if else statment.js
@@ -0,0 +1,81 @@
+let payment =true;
+
+if(payment){
+    console.log("payment done");                    //if condition always check true & print that
+
+}
+else{
+    console.log("payment not valid");
+}
+//////////////////////////////////////////////////////////////////////////////////////////
+let payment =true;
+let num = 10>10;
+let nbr = 10>=10;
+
+if(payment){
+    console.log("payment done");
+
+}
+else{
+    console.log("payment not valid");
+}
+
+
+if(num){
+    console.log("correct");
+
+}
+else{
+    console.log("wrong");
+}
+
+if(nbr){
+    console.log("correct");
+
+}
+else{
+    console.log("wrong");
+}}
+
+//////////////////////////////////////  Else If ////////////////////////////////////////////////////
+
+let whoIs = 'students';
+
+if (whoIs == 'admin')
+{
+    console.log('Hi Admin');
+}
+
+else if (whoIs == 'teachers')
+{
+    console.log('hi teachers');
+}
+
+else 
+{
+    console.log("hi students");
+}
+//////////////////////////////////////////////////////////////////////////////////////////
+
+let whoIs = 'fatheen';
+
+if (whoIs == 'admin')
+{
+    console.log('Hi Admin');
+}
+
+else if (whoIs == 'teachers')
+{
+    console.log('hi teachers');
+}
+
+else if (whoIs == 'students')
+{
+    console.log("hi students");
+}
+else 
+{
+    console.log("i don't know who you are");
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////
