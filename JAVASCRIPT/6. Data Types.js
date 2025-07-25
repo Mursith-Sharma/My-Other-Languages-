c5377d4fe65ac7ab2162cc8223db4db0528ef985
CYBER BOY ツ <mmursith12345@gmail.com>
2025-07-25 21:29:06 +0530

    Create 6. Data Types.js

---
+++ JAVASCRIPT/6. Data Types.js
@@ -0,0 +1,36 @@
+let nbr = 21;                                               // number
+let nmb = 2.1;                                              // ithil c programming pola float, int use panna thevaiyillai
+let text = 'Boss';                                          // string
+let octnum = 0255;
+let hexnum = 0xab;
+let amount = 1.1e6;                                          // 1.1 * 1000000  e6 entral 6 cyber udan multiple aagum
+let unlimit = 1.7976931348623157e+308 ;                     
+let unlimit1 = Number.MAX_VALUE + Number.MAX_VALUE;          // 1.7976931348623157e+308 ithu max value ithu 2 murai avruvathal infinity agirathu
+let unlimit2 = -2/0;
+let type = null;                                             // null
+let ronalso;                                                 // value illathathal undefind
+let make = true;                                             // boolean
+ 
+console.log(nbr);
+console.log(nmb);
+console.log(text);
+console.log(nmb + nbr);
+console.log(octnum);
+console.log(hexnum);
+console.log(amount);
+
+console.log(Number.MAX_VALUE);                                 //1.7976931348623157e+308    entraal antha number udan 308 zero udan muliple pannal varuvathu.
+console.log(Number.MIN_VALUE);              
+console.log(unlimit);              
+console.log(unlimit1);              
+console.log(unlimit2);              
+console.log(type);              
+console.log(ronalso);              
+console.log(make);              
+
+
+console.log(typeof make)                                       // data type inai kandupidiththal
+console.log(typeof type)                                       // once upon a time null nai object aagave use panninargal athanaal object entru kaattukirathu
+                                                               // ippothuthan athanai oru data type aaga use pannukirarkal
+
+
