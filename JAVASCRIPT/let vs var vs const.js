c63dc2b1aded725cf1c405c47fd0e70bd239cb8a
CYBER BOY ツ <mmursith12345@gmail.com>
2025-07-25 18:38:42 +0530

    Create let vs var vs const.js

---
+++ JAVASCRIPT/let vs var vs const.js
@@ -0,0 +1,30 @@
+//netflix il moview parkkavendumaayin athil login panniyirukka vendum & subscription pannirukka vendum athatkaana if else
+
+let name1 = 'true';
+const name2 = 'false';
+var name3 = 'var';
+
+name1 = 'true changed'
+//name2 = 'changed'                   //we can't change const variable name
+name3 = 'changed var'
+
+console.log(name1)
+console.log(name2)
+console.log(name3)
+console.log(club)                   //if condition il declar pannathai veliye use panna mudiyathu. but veliye declar panathai engum use pannalam
+                                    // but condition il "var" use pannal athai veliyeum use pannalam
+if(true){
+    var club = 'medrid'
+    console.log(name1);
+    console.log(name2);
+    console.log(name3);
+    console.log(name3 + club);
+}
+
+if(true){
+    var club = 'medrid';
+    console.log(club);
+}
+
+//  condition il "var" use pannal athai veliyeum use pannalam
+// ore var name inai palamurai use panna mudium (if condition il)
