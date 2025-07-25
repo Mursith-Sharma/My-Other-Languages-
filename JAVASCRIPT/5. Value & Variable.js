a777d30b748e17fd7f2bcbb92495e36466297bf8
CYBER BOY ツ <mmursith12345@gmail.com>
2025-07-25 21:04:46 +0530

    Create 5. Value & Variable.js

---
+++ JAVASCRIPT/5. Value & Variable.js
@@ -0,0 +1,25 @@
+let nbr = 21;
+let nmb = 2.1;
+let octnum = 0255;
+let hexnum = 0xab;
+let amount = 1.1e6;                                          // 1.1 * 1000000  e6 entral 6 cyber udan multiple aagum
+let unlimit = 1.7976931348623157e+308 ;                     
+let unlimit1 = Number.MAX_VALUE + Number.MAX_VALUE;           // 1.7976931348623157e+308 ithu max value ithu 2 murai avruvathal infinity agirathu
+let unlimit2 = -2/0;
+
+console.log(nbr);
+console.log(nmb);
+console.log(nmb + nbr);
+console.log(octnum);
+console.log(hexnum);
+console.log(amount);
+
+console.log(Number.MAX_VALUE);               //1.7976931348623157e+308    entraal antha number udan 308 zero udan muliple pannal varuvathu.
+console.log(Number.MIN_VALUE);              
+console.log(unlimit);              
+console.log(unlimit1);              
+console.log(unlimit2);              
+
+
+// ithil c programming pola float, int use panna thevaiyillai
+
