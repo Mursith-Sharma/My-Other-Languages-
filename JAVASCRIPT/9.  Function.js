13c9dd697f6682d3fe3ea01148ea5cfea2b6968a
CYBER BOY ツ <mmursith12345@gmail.com>
2025-07-26 20:39:35 +0530

    Create 9.  Function.js

---
+++ JAVASCRIPT/9.  Function.js
@@ -0,0 +1,19 @@
+
+let Hello = ('hi student');                           //Variable Declaring Method:
+console.log(Hello);
+
+////////////////////////////////////////////////////////////////
+
+function hello(){                                    //function method
+    console.log('hi students');
+}
+
+hello();                                             // function il ellavatraium programm pannivittu antha function name inai call pannaalthan output varum
+
+////////////////////////////////////////////////////////////////
+ 
+function helloo(name){                              //function method
+    console.log(`hi ${name}`);
+}
+
+helloo('Musith');                                  // website il oruvar avar name input pannal output il :- hi mursith varuvathu ivvaruthan 
