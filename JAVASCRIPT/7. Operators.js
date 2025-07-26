ee31a64c3877d7d8c4508de64730dd0494d0d44d
CYBER BOY ツ <mmursith12345@gmail.com>
2025-07-26 10:45:19 +0530

    Update and rename 7. Operators to 7. Operators.js

---
+++ JAVASCRIPT/7. Operators.js
@@ -0,0 +1,51 @@
+arithmatic operators (ithai keywords or sympoles aagavum use pnnalam)
+1. addition
+2. subtraction
+2. multiplication
+4. divition
+5. modules/ reminders
+
+////////////////////////////////////////////////////////////////////////////
+
+let result = 20 + 10;
+console.log(result);        //30
+
+result = result -20;
+console.log(result);        //10
+
+result = result + 20;
+console.log(result);        //30
+
+result = result / 3;
+console.log(result);        //10
+
+result = result * 2;
+console.log(result);        //20
+ 
+result = result % 3;
+console.log(result);        //2
+console.log('////////////////////////');        //2
+
+/////////////////////////////// unary operator //////////////////////////////
+
+let nbr = 10;
+
+marks = ++nbr           
+console.log(nbr);       //11
+
+marks = nbr++      
+console.log(nbr);       //12
+
+marks = nbr--          
+console.log(nbr);       //11
+
+marks = --nbr
+console.log(nbr);       //10
+
+console.log(marks);      // answer = 10  
+
+/////////////////////////////////////////////////////////////
+
+let text = 'Javascript';
+console.log('hello ' + text + '! ' + 404);            // ithil operator aanathu number inai string aaga matrum
+console.log(200 - 'dob');                             // NAN output varum (not a number)
