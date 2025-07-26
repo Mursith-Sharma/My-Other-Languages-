9ddff0846a59f100d6be3d1211459bfaf547f13c
CYBER BOY ツ <mmursith12345@gmail.com>
2025-07-26 13:48:40 +0530

    Update 7. Operators.js

--- JAVASCRIPT/7. Operators.js
+++ JAVASCRIPT/7. Operators.js
@@ -60,6 +60,44 @@ console.log(16 != 16);         //false
 console.log(16 == '16');       //true       en number inaium string inaium equal entru solkirathu:- bcz relational operator string inai number aaga convert pannum
 console.log(16 != '16');       //false
 
+////////////////////////////////////////////////////////////////
+//Not Operator (ethavathu ontru true aayiun true than answer)
+
+console.log(!true);                      // false
+console.log(!false);                    //true
+console.log(true || false);             // true
+console.log(false || false);            // false
+console.log(true || true);             // true
+console.log(!false || false);          // true
+console.log(false || !true);          // false
+console.log(null || true);           // true
+console.log('' || 25);               // 25
+console.log('hi' || null);           // hi
+
+console.log('/////////////////////////////////////////////////////');
+//AND Operator (2 um true aayiun true than answer)            
+
+console.log(true && false);             // false
+console.log(false && false);            // false
+console.log(true && true);             // true
+console.log(!false && true);           // true
+console.log(false && !true);           // false
+console.log('' && !true);          
+console.log('hi' && true);            // true for reason athil somthing value ontru ullathu
+console.log('hi' && null);            //null (1 mattum true aahaiyal null print aagum)
+
+console.log('/////////////////////////////////////////////////////');
+
+console.log(!10);                     // false for reason 10 enpathu true but !10 enpathu not true aahaiyal false
+console.log(!'ok');                  // false for reason ok enpathu true but !10 enpathu not true aahaiyal false
+console.log(!0);                     // true for reason 0 enpathu emty but !0 enpathu not emty(value ullathu entru arththam) aahaiyal true
+console.log(!null);                  // null entral value illai but !null entral athil value ullathu so true
+
+console.log('/////////////////////////////////////////////////////');
+
+console.log(undefined || false);     //false
+console.log(undefined || false);     //false
+
 console.log(16 === '16');       //false     so data convertion aagamal irukkka java il extra aaha = poduvom
 console.log(16 !== '16');       //false
 
