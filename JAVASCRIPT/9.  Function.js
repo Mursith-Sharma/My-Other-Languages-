831379294584926689e234cc1efadb64ef926dbb
CYBER BOY ツ <mmursith12345@gmail.com>
2025-07-26 20:54:53 +0530

    Update 9.  Function.js

--- JAVASCRIPT/9.  Function.js
+++ JAVASCRIPT/9.  Function.js
@@ -17,3 +17,41 @@ function helloo(name){                              //function method
 }
 
 helloo('Musith');                                  // website il oruvar avar name input pannal output il :- hi mursith varuvathu ivvaruthan 
+
+////////////////////////////////////////////////////////////////
+
+
+let Hello = ('hi student');                           //Variable Declaring Method:
+console.log(Hello);
+
+////////////////////////////////////////////////////////////////
+
+function hello(){                                    //function method
+    console.log('hi students');
+}
+
+hello();                                             // function il ellavatraium programm pannivittu antha function name inai call pannaalthan output varum
+
+////////////////////////////////////////////////////////////////
+ 
+function helloo(name){                              //function method
+    console.log(`hi ${name}`);
+}
+
+helloo('Musith');                                  // website il oruvar avar name input pannal output il :- hi mursith varuvathu ivvaruthan 
+
+////////////////////////////////////////////////////////////////
+
+function course(name1 , name2){                              // login form il 1st & last name ivvaru ketkalam
+    console.log(`hi ${name1} ${name2} welcome to our course`);
+}
+
+course('aathil','ahamed');   
+
+////////////////////////////////////////////////////////////////
+
+function add(num1 , num2){                              // login form il 1st & last name ivvaru ketkalam
+    console.log(num1 + num2);
+}
+
+add(20+30/2 , 60);   
