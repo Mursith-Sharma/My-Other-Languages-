f1a1b901639d49e8e3bf7d26afee4307df15b89b
CYBER BOY ツ <mmursith12345@gmail.com>
2025-07-27 19:28:01 +0530

    Update 15. Search.js

--- JAVASCRIPT/15. Search.js
+++ JAVASCRIPT/15. Search.js
@@ -4,3 +4,14 @@ console.log(hero.indexOf('thor'))                // position number kattum
 console.log(hero.indexOf('LOKI'))
 
 ////////////////////////////////////////////////////////////////////////////////////////
+
+let hero = ['hulk','SPider','loki','marvel','thor'];
+
+console.log(hero.find(function(value)                   
+{  
+     console.log(value.toLowerCase() === 'spider')             //oruvar spider entru search pannal ivvaru match aagum and tolower koduththal capital il search pannalum lower convert aahi match aagum
+} 
+)) 
+//match aahinal true um match aagavittal false um kattum
+// 'hulk'.toLowerCase() === 'spider' → 'hulk' === 'spider' → false
+// 'SPider'.toLowerCase() === 'spider' → 'spider' === 'spider' → ✅ true
