b3b894f961bceb8e0f0867d6fd4e99f7eaef2b7f
CYBER BOY ツ <mmursith12345@gmail.com>
2025-07-27 10:50:28 +0530

    Update 11. Loob.js

--- JAVASCRIPT/11. Loob.js
+++ JAVASCRIPT/11. Loob.js
@@ -1,1 +1,26 @@
-m
+////////////////////////////////////////////////  for loob  /////////////////////////////////////////////////////
+
+//for loob => for (intiator/variablr; condition; increment/decrment)
+
+for(my = 0; my < 10; my = my + 1)
+{
+    console.log(my);
+}
+
+console.log('///////////////////////////////////////////////////')
+
+let hero = ['hulk', 'spider', 'loki'];
+
+for(i=0; i<3; i++)
+{
+    console.log(i+1 , hero[i]);
+}
+
+console.log('///////////////////////////////////////////////////')
+
+let heros = ['hulk', 'spider', 'loki','iron man', 'thanos'];           //auto aaha id set aahum
+
+for(i=0; i<heros.length; i++)
+{
+    console.log(i+1 , heros[i]);
+}
