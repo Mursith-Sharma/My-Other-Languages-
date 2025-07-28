e6689d90462ef0fbe93d64406dc1e6befae8c593
CYBER BOY ツ <mmursith12345@gmail.com>
2025-07-28 11:22:08 +0530

    Update 17. Filter.js

--- JAVASCRIPT/17. Filter.js
+++ JAVASCRIPT/17. Filter.js
@@ -1,15 +1,34 @@
 
+let bye = function(total){
+    return(`bye ${total}`)
+};
+console.log(bye('ihsan'));
+
+//////////////////////////// advanced way use arrow ///////////////////////////////
+
+let hello = (total) => {
+    return(`bye ${total}`)               // mostly programmers function kku pathilaaha arrow mark => use pannuvar 2 um same work than pannum
+};
+console.log(hello('ihsan'));
+
+/////////////////////////////// single line using arrow //////////////////////////////////////
+
+let hi = (total) =>`bye ${total}`;           // simple function kku ithu use pannalam
+console.log(hi('ihsan'));
+
+////////////////////////////////////////////// Filter start /////////////////////////////////////////////////////
+
 let mytoday = [{
     title : 'prayer',
-    isdone : 'true'
+    isdone : true,
 },
 {
     title : 'go to gym',
-    isdone : 'false'
+    isdone : false,
 },
 {
     title : 'play game',
-    isdone : 'true',
+    isdone : true,
 }];
 
 console.log(mytoday[1]);
@@ -18,19 +37,19 @@ console.log(mytoday[1]);
 
 let today = [{
     title : 'prayer',
-    isdone : 'true'
+    isdone : true,
 },
 {
     title : 'go to gym',
-    isdone : 'false'
+    isdone : false,
 },
 {
     title : 'play game',
-    isdone : 'false',
+    isdone : false,
 },
 {
     title : 'eat',
-    isdone : 'true',
+    isdone : true,
 }];
 
 let find = today.filter(function(value){                       // value il ovvoru posititon um sellum 0,1,2 
