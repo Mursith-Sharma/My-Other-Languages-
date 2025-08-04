c4293bb7685b704e455394d9235994870c440cd1
CYBER BOY ツ <mmursith12345@gmail.com>
2025-08-04 12:36:44 +0530

    Create 1. Setup (react native) .jsx

---
+++ REACT NATIVE/1. Setup (react native) .jsx
@@ -0,0 +1,24 @@
+1. 🔹 React Native (Mobile App frontend):
+100% Node.js தேவை.
+ஏனெனில் React Native:
+npm (Node Package Manager) பயன்படுத்துகிறது.
+Expo CLI / React Native CLI இரண்டும் Node.js மூலம் தான் வேலை செய்கின்றன.
+
+2. ithanudan npm = Node Package Manager install aahi irukkum.
+athai 2um check panna cmd il ( node -v & npm -v )
+
+3. go to project folder path, click path , type cmd, enter
+
+4. go to browser, go to vite website, copy this commend => npm create vite@latest your-project-name, paste in your cmd (3), enter, y,  react, select fraamework(react) , javascript
+
+5. (node modules files crearte pannanum so)  cd React , npm install
+
+6. (now project inai run panna) npm run dev
+
+7. so cmd il development server iruppathall athu stop aahinal antha host work aahathu. so marupadium run panna  same location il npm run dev kodukka vendum
+
+8. open your project folder vs code
+
+9. now neenal cmd il host inai run panna thevaiyillai, so go to vs terminal, npm run dev , error vanthal run this => Set-ExecutionPolicy RemoteSigned -Scope CurrentUser , npm run dev. done
+
+10. public folder il html,css files irukkum, app.jsx file il your host url il irukkum interface kkana code irukkkum you can edit, main.jsx: App.jsx-ஐ DOM-ல் render செய்யும் (React-ன் entry point).
