e0e0f12a128350f6b3c19a65fdfdf6b94ac952d3
CYBER BOY ツ <mmursith12345@gmail.com>
2025-08-21 14:16:23 +0530

    Update 9.  Function.js

--- JAVASCRIPT/9.  Function.js
+++ JAVASCRIPT/9.  Function.js
@@ -74,3 +74,93 @@ console.log(hello('ihsan'));
 
 let hi = (total) =>`bye ${total}`;           // simple function kku ithu use pannalam
 console.log(hi('ihsan'));
+
+/////////////////////////////////////////////////////////////////////////////////  BIGGINING  ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+let Hello = ('hi student');                           //Variable Declaring Method: let or const or var use panlam but diffrent irukku
+console.log(Hello);
+
+let math = (3 + 5.8);                         // c programming pola datatype mantion panna thevaiyillai like int, float
+console.log(math);
+                                        // data type use pannum   language  C, C#, C++, JAVA, GO
+                                        // data type use pannatha language JAVASCRIPT, PHP, PYTHON
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+function hello()
+{
+ console.log('yeah buddy');
+}
+hello();
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
+
+function num(var1,var2){
+    console.log( var1 + var2);        
+}
+
+num(6,6);
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
+
+let a=4, b=5;
+function num(a,b){
+    console.log(a + b);                  // type 1
+}                                                     // intha 2 type ilum code pannalam
+
+num(a,b);
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
+
+let a=4, b=5;
+function num(var1,var2){
+    console.log( var1 + var2);           // type 2
+}
+
+num(a,b);
+
+/*
+naan 2 data eduththullen, athai function moolam var1, var2 enum parameter aaha pass pannukiren
+athai argument aaha datavinai vanguki  outpot pannukirathu
+Arguments passing: a = 4, b = 5
+Function-ல் var1 = 4, var2 = 5 ஆக ஆகிறது.
+Argument என்பது function-ஐ call செய்யும்போது function-க்கு அனுப்பப்படும் value ஆகும்.
+pin naam eppothu call pannukiromo appothuthan namakku output varum
+*/
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+                                                        Biggest Advantage Of Function
+
+///////////////////////////////////////////////////////
+let a = 4.4;
+let b = 5.6;
+
+// a + b
+let sum1 = a + b;
+console.log("answer: " + sum1);
+
+let c = 10;
+let d = 20;                                            // WITHOUT FUNCTION
+
+// c + d
+let sum2 = c + d;
+console.log("answer: " + sum2);
+
+let e = 1.5;
+let f = 2.5;
+
+// e + f
+let sum3 = e + f;
+console.log("answer: " + sum3);
+
+///////////////////////////////////////////////////////
+
+function addNumbers(x, y) {
+    console.log("answer: " + (x + y));
+}
+                                                         // WITH FUNCTION
+addNumbers(4.4, 5.6);
+addNumbers(10, 20);
+addNumbers(1.5, 2.5);
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
